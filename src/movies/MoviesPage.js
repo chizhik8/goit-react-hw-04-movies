1098576eaf70ebc030625c918d01e71e5b24511e
julia_chizhik <for.yu.ch@gmail.com>
2021-01-27 18:32:26 +0200

    search

--- src/movies/MoviesPage.js
+++ src/movies/MoviesPage.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import queryString from 'query-string';
 import Searcher from '../components/Searcher';
-
+import moviesAPI from '../services/moviesApi';
 
 
 export class MoviesPage extends Component {
 
-    state = {
+    state = { movies: []};
 
-    };
-    
+    componentDidUpdate(prevProps, prevState) { 
+        const {query: prevQuery} = queryString.parse(prevProps.location.search);
+        const {query: nextQuery} = queryString.parse(this.props.location.search);
+        if (prevQuery !== nextQuery) {
+            moviesAPI.fetchMoviesSearch(nextQuery)
+            .then(movies => this.setState({movies}))
+            .catch(error => console.log(error))
+         }
+    }
+
+
     handleChangeQuery = query => { 
-        console.log(query);
         this.props.history.push({
             pathname: this.props.location.pathname,
             search: `query=${query}`
@@ -18,9 +28,13 @@ export class MoviesPage extends Component {
     }
 
     render() {  
+        
         return (
             <div>
                 <Searcher onSubmit={this.handleChangeQuery} />
+                <ul className='trendingList'>
+                    {this.state.movies.map(mov => (<li key={mov.id}><Link to={`/movies/${mov.id}`}>{mov.title}</Link></li>) )}
+                </ul>  
             </div>
         )
     }
